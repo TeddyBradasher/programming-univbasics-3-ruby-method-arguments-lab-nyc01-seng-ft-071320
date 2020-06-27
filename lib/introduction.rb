10d63a6be250bc19e6c36ea8ae551564d278425f
Teddy Bradsher <teddybradsher@gmail.com>
2020-06-27 20:48:36 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,10 +11,7 @@ def introduction_with_language (name, language)
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language_optional (name, language)
-when language == "Ruby"
-puts "Hi, my name is #{name} and I am learning to program in Ruby."
-else
+introduction_with_language_optional (name, language = ruby)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
